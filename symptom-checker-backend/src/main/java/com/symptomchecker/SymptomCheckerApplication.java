e65e822308cb1b65392e484f470475f53510e61d
Maaz <mmk.maaz@gmail.com>
2026-06-11 23:53:01 -0500

    fixing backend

--- symptom-checker-backend/src/main/java/com/symptomchecker/SymptomCheckerApplication.java
+++ symptom-checker-backend/src/main/java/com/symptomchecker/SymptomCheckerApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.symptomchecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
