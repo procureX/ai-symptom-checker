560995d0bb75f9bb2ee52feb2685a5c0c795de24
Maaz <mmk.maaz@gmail.com>
2026-06-11 23:56:30 -0500

    fixing class name

--- symptom-checker-backend/src/main/java/com/symptomchecker/SymptomCheckerApplication.java
+++ symptom-checker-backend/src/main/java/com/symptomchecker/SymptomCheckerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SymptomCheckerBackendApplication {
+public class SymptomCheckerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SymptomCheckerBackendApplication.class, args);
